232f077c74f2064d3e4f84a19aea94528c592566
Rubia Souza <rubensvictor.m.s@gmail.com>
2021-08-03 21:01:34 -0300

    feat(assets): Add project fonts

---
+++ OwRemedio/src/shared/utils/AssetsReferences/Fonts.ts
@@ -0,0 +1,18 @@
+const Fonts = {
+    Roboto: {
+        black: "Roboto-Black",
+        blackItalic: "Roboto-BlackItalic",
+        bold: "Roboto-Bold",
+        boldItalic: "Roboto-BoldItalic",
+        medium: "Roboto-Medium",
+        mediumItalic: "Roboto-MediumItalic",
+        regular: "Roboto-Regular",
+        regularItalic: "Roboto-Italic",
+        light: "Roboto-Light",
+        lightItalic: "Roboto-LightItalic",
+        thin: "Roboto-Thin",
+        thinItalic: "Roboto-ThinItalic",
+    },
+};
+
+export default Fonts;
