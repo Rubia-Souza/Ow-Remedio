117d8a8568bc277541f272b910026a9f49b1b81b
Rubia Souza <rubensvictor.m.s@gmail.com>
2021-08-07 20:50:49 -0300

    feat(utils): Create Med Interface

--- OwRemedio/src/shared/dto/Med.ts
+++ OwRemedio/src/shared/dto/Med.ts
@@ -9,4 +9,22 @@ interface Med {
     hasSound: boolean;
 }
 
+export class MedUtils {
+    static renderTime(medData: Med): string {
+        if(!medData || !medData.time) {
+            return "";
+        }
+    
+        return `${medData.time.getHours()}:${medData.time.getMinutes()}`;
+    }
+
+    static renderStock(medData: Med): string {
+        if(!medData) {
+            return "";
+        }
+
+        return `${medData.actualStock}/${medData.maxStock}`;
+    }
+}
+
 export default Med;
