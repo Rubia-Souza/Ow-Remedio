bd71df6c8f6b49df99ecb84fe57c8dae58bb304b
Rubia Souza <rubensvictor.m.s@gmail.com>
2021-08-07 20:49:10 -0300

    feat(navigation): Create RootNavigation

---
+++ OwRemedio/src/navigation/RootNavigator/RootNavParamsList.ts
@@ -0,0 +1,3 @@
+export interface RootNavParamsList {
+    StackNavigator: undefined;
+};
---
+++ OwRemedio/src/navigation/RootNavigator/index.ts
@@ -0,0 +1,5 @@
+import RootNavigator from "./RootNavigator";
+export * from "./RootNavParamsList";
+export * from "./RootNavScreenData";
+
+export default RootNavigator;
