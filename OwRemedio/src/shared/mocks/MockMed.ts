07aeb54cb10d5448650383fa00a70203c2c47f4e
Rubia Souza <rubensvictor.m.s@gmail.com>
2021-08-15 15:51:30 -0300

    refactor(mocks): Change mocks to use the Builder

--- OwRemedio/src/shared/mocks/MockMed.ts
+++ OwRemedio/src/shared/mocks/MockMed.ts
@@ -1,20 +1,16 @@
-import Med from "../dto/Med";
+import Med, { MedBuilder } from "../dto/Med";
 import NotificationTypes from "../dto/NotificationTypes";
 
-export const Med1: Med = {
-    name: "Buscopan",
-    time: new Date(0,0, 0, 12, 30, 0),
-    maxStock: 20,
-    actualStock: 10,
-    hasSound: true,
-    notificationType: NotificationTypes.Alarm,
-};
+export const Med1: Med = new MedBuilder("Buscopan")
+    .withTime(new Date(0,0, 0, 12, 30, 0))
+    .withMaxStock(20)
+    .withActualStock(10)
+    .thatHasSound(true)
+    .withNotificationType(NotificationTypes.Alarm).build();
 
-export const Med2: Med = {
-    name: "Flogoral",
-    time: new Date(0,0, 0, 18, 0, 0),
-    maxStock: 30,
-    actualStock: 5,
-    hasSound: false,
-    notificationType: NotificationTypes.Push,
-};
+export const Med2: Med = new MedBuilder("Flogoral")
+    .withTime(new Date(0,0, 0, 18, 0, 0))
+    .withMaxStock(30)
+    .withActualStock(5)
+    .thatHasSound(false)
+    .withNotificationType(NotificationTypes.Push).build();
