89d1cc8072bab490934720ec5cb35553f8d4cc1c
Rubia Souza <rubensvictor.m.s@gmail.com>
2021-08-15 15:46:15 -0300

    fix(component): Set DefaultTitle default props

--- OwRemedio/src/shared/components/Title/DefaultTitle.tsx
+++ OwRemedio/src/shared/components/Title/DefaultTitle.tsx
@@ -4,13 +4,13 @@ import { Text } from "react-native";
 import styles from "./styles";
 
 interface DefualtTitleProps {
-    style: any;
-    children: any;
+    style?: any;
+    children?: any;
 }
 
 const DefualtTitle: React.FC<DefualtTitleProps> = ({
-    style,
-    children,
+    style = {},
+    children = null,
 }) => {
     return (
         <Text style={[styles.DefualtTitle, {...style}]}>
