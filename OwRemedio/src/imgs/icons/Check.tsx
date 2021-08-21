effa04917f9d2dda352882d8a9306c0d4f9afbd3
Rubia Souza <rubensvictor.m.s@gmail.com>
2021-08-21 00:51:58 -0300

    refactor(icons): Add props inheritance

--- OwRemedio/src/imgs/icons/Check.tsx
+++ OwRemedio/src/imgs/icons/Check.tsx
@@ -8,24 +8,31 @@ interface CheckProps extends SvgProps {
     color?: string;
 }
 
-const Check: React.FC<CheckProps> = ({
-    width = "512",
-    height = "512",
-    color = Colors.black2E,
-}) => {
+const initialState: CheckProps = {
+    width: "30",
+    height: "30",
+    color: Colors.black2E,
+};
+
+const Check: React.FC<CheckProps> = (
+    props: CheckProps
+) => {
     return (
         <Svg
-            width={width}
-            height={height}
+            width={props.width}
+            height={props.height}
             viewBox="0 0 21 21"
             fill="none"
+            {...props}
         >
             <Path
                 d="M18.129 2.838c-.115.029-1.157 1.054-5.07 4.96l-4.926 4.921-2.514-2.543c-1.73-1.751-2.572-2.572-2.686-2.63a.842.842 0 00-.669-.02C2.059 7.613.184 9.475.078 9.7a.766.766 0 000 .657c.102.233 7.506 7.674 7.735 7.776.21.099.448.099.657 0 .233-.11 12.378-12.268 12.469-12.481a.778.778 0 00-.017-.628c-.107-.225-1.981-2.087-2.186-2.173a.952.952 0 00-.607-.013z"
-                fill={color}
+                fill={props.color}
             />
         </Svg>
     );
 };
+
+Check.defaultProps = initialState;
 
 export default Check;
--- OwRemedio/src/imgs/icons/Plus.tsx
+++ OwRemedio/src/imgs/icons/Plus.tsx
@@ -8,24 +8,31 @@ interface PlusProps extends SvgProps {
     color?: string;
 }
 
-const Plus: React.FC<PlusProps> = ({
-    width = "512",
-    height = "512",
-    color = Colors.black2E,
-}) => {
+const initialState: PlusProps = {
+    width: "30",
+    height: "30",
+    color: Colors.black2E,
+};
+
+const Plus: React.FC<PlusProps> = (
+    props: PlusProps
+) => {
     return (
         <Svg
-            width={width}
-            height={height}
+            width={props.width}
+            height={props.height}
             viewBox="0 0 32 32"
             fill="none"
+            {...props}
         >
             <Path
                 d="M15.444.069c-1.144.225-2.088 1.231-2.257 2.4-.037.256-.062 2.481-.062 5.381v4.944l-.162.168-.17.163H7.85c-2.9 0-5.125.025-5.381.063-1.55.218-2.669 1.718-2.419 3.23a2.878 2.878 0 002.419 2.395c.262.037 2.4.062 5.381.062h4.944l.168.163.163.168v4.944c0 2.9.025 5.125.063 5.381.218 1.544 1.718 2.669 3.225 2.419 1.256-.206 2.224-1.188 2.4-2.419.037-.256.062-2.481.062-5.381v-4.944l.163-.168.168-.163h4.944c2.981 0 5.119-.025 5.381-.063 1.556-.225 2.669-1.712 2.419-3.23a2.878 2.878 0 00-2.419-2.395c-.262-.037-2.4-.062-5.381-.062h-4.944l-.168-.162-.163-.17V7.85c0-2.981-.025-5.119-.063-5.381-.237-1.613-1.78-2.713-3.368-2.4z"
-                fill={color}
+                fill={props.color}
             />
         </Svg>
     );
 };
+
+Plus.defaultProps = initialState;
 
 export default Plus;
