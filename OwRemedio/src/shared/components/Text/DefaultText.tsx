16211231157e1fb3216a65c07dc87d8cb3688013
Rubia Souza <rubensvictor.m.s@gmail.com>
2021-08-29 20:27:46 -0300

    refactor(components): Change text to show it's original props

--- OwRemedio/src/shared/components/Text/DefaultText.tsx
+++ OwRemedio/src/shared/components/Text/DefaultText.tsx
@@ -1,22 +1,31 @@
 import React from "react";
-import { Text } from "react-native";
+import { 
+    Text,
+    TextProps,
+} from "react-native";
 
 import styles from "./styles";
 
-interface DefualtTextProps {
+interface DefualtTextProps extends TextProps {
     style?: any;
     children?: any;
 }
 
-const DefualtText: React.FC<DefualtTextProps> = ({
-    style = {},
-    children = null,
-}) => {
+const initialState: DefualtTextProps = {
+    style: {},
+    children: null,
+};
+
+const DefualtText: React.FC<DefualtTextProps> = (
+    props: DefualtTextProps
+) => {
     return (
-        <Text style={[styles.DefualtText, {...style}]}>
-            {children}
+        <Text style={[{...styles.DefualtText}, {...props.style}]} {...props}>
+            {props.children}
         </Text>
     );
 };
+
+DefualtText.defaultProps = initialState;
 
 export default DefualtText;
