c18f4eaa98826ec61ba98b6710b0e1a3fa8eee9d
Rubia Souza <rubensvictor.m.s@gmail.com>
2021-09-25 17:33:12 -0300

    feat(components): Add export to buttons index

--- OwRemedio/src/shared/components/Buttons/index.ts
+++ OwRemedio/src/shared/components/Buttons/index.ts
@@ -1,1 +1,6 @@
 export * from "./AddButton/AddButton";
+export * from "./DefaultButton/DefaultButton";
+export * from "./GhostButton/GhostButton";
+
+export type { DefaultButtonElementsStyles, } from "./DefaultButton/styles";
+export type { GhostButtonElementsStyles, } from "./GhostButton/styles";
