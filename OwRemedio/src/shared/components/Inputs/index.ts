e857a0505e2d488b45182c54c78ca4df3abf113f
Rubia Souza <rubensvictor.m.s@gmail.com>
2021-09-25 21:41:38 -0300

    feat(components): Update input index exportations

--- OwRemedio/src/shared/components/Inputs/index.ts
+++ OwRemedio/src/shared/components/Inputs/index.ts
@@ -1,1 +1,15 @@
 export * from "./RadioButton/RadioButton";
+export * from "./TextInput/DefaultTextInput";
+export * from "./NumberInput/DefaultNumberInput";
+export * from "./DateInput/DefaultDateInput";
+export * from "./TimeInput/DefaultTimeInput";
+export * from "./CheckBox/Checkbox";
+export * from "./DropDown/DropDow";
+
+export type { CheckboxElementsStyle, } from "./CheckBox/styles";
+export type { DateInputElementsStyles, } from "./DateInput/styles";
+export type { DropDownElementsStyles, } from "./DropDown/styles";
+export type { DefaultNumberInputElementsStyles, } from "./NumberInput/styles";
+export type { RadioButtonStyles, } from "./RadioButton/styles";
+export type { DefaultTextInputElementsStyles, } from "./TextInput/styles";
+export type { DefaultTimeInputElementStyles, } from "./TimeInput/styles";
